1df3c60f882f91989501c4f62bdf98763e3292f8
candycrushpro <bradleynilmar6702@gmail.com>
2020-04-20 15:25:51 +0300

    test case to show if credentials objects have been saved

--- credentials_test.py
+++ credentials_test.py
@@ -1,8 +1,8 @@
-import unnittest
-from credentials import credentials
+import unittest
+from credentials import Credentials
 
 
-class TestCredentials(unittest.Testcase):
+class TestCredentials(unittest.TestCase):
 
     '''
     test class that defines test cases for the credentials class
@@ -20,3 +20,49 @@ class TestCredentials(unittest.Testcase):
         tear down method that does clean up after each test case has run.
         '''
         Credentials.credentials_list = []
+
+    def test_credentials_instance(self):
+        '''
+        method to test if new credentials have been instanciated properly
+        '''
+        self.assertEqual(self.new_credentials.account_name,"Kijakazi")
+        self.assertEqual(self.new_credentials.account_password,"6702")
+
+    def test_save_credentials(self):
+        '''
+        test case to test if credentials objects have been saved
+        '''
+
+        self.new_credentials.save_credentials() # save new user
+        self.assertEqual(len(Credentials.credentials_list),1)
+
+    def test_delete_credentials(self):
+        '''
+        test delete credentials to test if we can remove an account from our list
+        '''
+
+        self.new_credentials.save_credentials()
+        test_credentials = Credentials("Kijakazi","6702")
+        test_credentials.save_credentials()
+        self.new_credentials.delete_credentials() # to delete a credentials object
+        self.assertEqual(len(Credentials.credentials_list),1)
+
+    def test_find_credentials_by_name(self):
+        '''
+        test to check if credentials by the account name and can display information
+        '''
+
+        self.new_credentials.save_credentials()
+        test_credentials = Credentials("Kijakazi","6702")
+        test_credentials.save_credentials()
+
+    def test_display_all_credentials(self):
+        '''
+        Test to check if all contacts can be viewed
+        '''
+
+        self.assertEqual(Credentials.display_credentials(),Credentials.credentials_list)
+                         
+
+if __name__ == '__main__':
+    unittest.main()
