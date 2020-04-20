c09af1f5c38fd9c4ddd82d6c76d169756d98db28
candycrushpro <bradleynilmar6702@gmail.com>
2020-04-20 05:01:58 +0300

    add a new user in module user_test.py

--- user_test.py
+++ user_test.py
@@ -17,5 +17,25 @@ class TestUser(unittest.TestCase):
 
         self.new_user = User("Bradley","password")
 
+    def test_init_(self):
+        '''
+        test case to if the User object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.user_name,"Bradley")
+        self.assertEqual(self.new_user.password,"password")
+
+    def test_save_user(self):
+        '''
+        test case to check if the user is able to save the name and password 
+        ''' 
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
+
+
+if __name__ =='__main__':
+    unittest.main()
+
+
     
       
